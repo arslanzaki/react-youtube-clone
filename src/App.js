f9c53f0b23b19ab579fbfbc0c9306799dfc5ea3d
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-08 18:37:28 +0500

    Routes Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+// import Navbar  from "./components/Navbar";
+// import Feed from "./components/Feed";
+// import VideoDetail from "./components/VideoDetail";
+// import ChannelDetail from "./components/ChannelDetail";
+// import SearchFeed from "./components/SearchFeed";
+
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000", color: '#fff' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
