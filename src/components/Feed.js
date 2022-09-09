6abb80f62139cd914ab9f21bfae4e1c62bcba24a
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-09 17:59:43 +0500

    SideBar UI Complete

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Stack, Typography, Box } from "@mui/material";
+import { borderRight } from "@mui/system";
+import Sidebar from "./SideBar";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sm: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sm: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sm: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5 }}>
+          Copyright 2022 YT Clone
+        </Typography>
+      </Box>
 
-export default Feed
+    
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/components/SideBar.js
@@ -0,0 +1,45 @@
+import { Stack } from "@mui/system";
+import React from "react";
+import { categories } from "../utils/constants";
+
+const selectedCategory = "New";
+
+const Sidebar = () => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sm: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button
+          key={category.name}
+          className="category-btn"
+          style={{
+            background: category.name === selectedCategory && "#FC1503",
+            color: "#FFFFFF",
+          }}
+        >
+          <span
+            style={{
+              color: category.name === selectedCategory ? "#FFFFFF" : "#FC1503",
+              marginRight: "15px",
+            }}
+          >
+            {category.icon}
+          </span>
+          <span
+            style={{ opacity: category.name === selectedCategory ? 1 : 0.8 }}
+          >
+            {category.name}
+          </span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
