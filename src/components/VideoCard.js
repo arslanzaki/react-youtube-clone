0a0da911b7419620a905388adbc4c2f3c241cf07
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-13 00:04:35 +0500

    Video Card UI Done

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -18,10 +18,24 @@ const VideoCard = ({
 }) => {
   //console.log(videoId, snippet)
   return (
-    <Card>
+    <Card sx={{width :{md: '320px', xs: '100%'}, boxShadox: 'none', borderRadius: 0}}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} sx={{width: 358, height: 180}} />
         </Link>
+        <CardContent sx={{backgroundColor: '#1e1e1e', height:'106px'}}>
+            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <Typography variant="subtitle1" fontWeight="bold" color="#ffffff">
+                    {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+                </Typography>
+            </Link>
+
+            <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                <Typography variant="subtitle2" fontWeight="bold" color="gray">
+                    {snippet?.channelTitle || demoChannelTitle}
+                    <CheckCircle sx={{fontSize: 12, color: 'gray', ml: '5px'}}/>
+                </Typography>
+            </Link>
+        </CardContent>
     </Card>
   )
 };
