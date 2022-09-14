7a1b67ef383fe04efbacd3d726969f5bb05fc018
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-14 23:10:32 +0500

    ChannelDetails API Setup

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const { id } = useParams();
 
-export default ChannelDetail
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [channelVideos, setChannelVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setChannelVideos(data?.items)
+    );
+  }, [id]);
+
+  console.log(channelDetail, channelVideos);
+
+  return <div>ChannelDetail</div>;
+};
+
+export default ChannelDetail;
